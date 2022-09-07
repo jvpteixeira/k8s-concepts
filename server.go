166a39b64e8bce97f79ecad3fd097568c0339864
João Victor Teixeira <48188729+JoaoVictorPereiraTeixeira@users.noreply.github.com>
2022-09-07 17:35:32 -0300

    Stress tests with fortio

--- server.go
+++ server.go
@@ -28,13 +28,13 @@ func Secret(w http.ResponseWriter, r *http.Request) {
 
 func Healthz(w http.ResponseWriter, r *http.Request) {
 	duration := time.Since(startedAt)
-
-	if duratin.Seconds() < 10 || duration.Seconds() > 30 {
-		w.WriteHeader(500)
-		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+   
+	if duration.Seconds() < 10 {
+	  w.WriteHeader(500)
+	  w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
 	} else {
-		w.WriteHeader(200)
-		w.Write([]byte("ok"))
+	  w.WriteHeader(200)
+	  w.Write([]byte("ok"))
 	}
 }
 
