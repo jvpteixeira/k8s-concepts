8bc862e1403943699d48522f036134a39815099f
João Victor Teixeira <48188729+JoaoVictorPereiraTeixeira@users.noreply.github.com>
2022-09-30 17:11:45 -0300

    Preparing tech talk

---
+++ serverv1.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"	"os"
+)
+
+
+func main() {
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":80", nil)
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s", name, age)
+}
